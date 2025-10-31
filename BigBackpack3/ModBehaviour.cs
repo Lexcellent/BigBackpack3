39b2e9ec05a57f868963e34c0dae82093199b2d1
Lexcellent <2587936056@qq.com>
2025-11-01 01:41:14 +0800

    功能完成

---
+++ BigBackpack3/ModBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using ItemStatsSystem;
+using ItemStatsSystem.Stats;
+
+namespace BigBackpack3
+{
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        protected override void OnAfterSetup()
+        {
+            LevelManager.OnAfterLevelInitialized -= OnAfterLevelInitialized;
+            LevelManager.OnAfterLevelInitialized += OnAfterLevelInitialized;
+        }
+
+        protected override void OnBeforeDeactivate()
+        {
+            LevelManager.OnAfterLevelInitialized -= OnAfterLevelInitialized;
+        }
+
+        void OnAfterLevelInitialized()
+        {
+            // 背包物品变化
+            LevelManager.Instance.MainCharacter.CharacterItem.Inventory.onContentChanged -= OnContentChanged;
+            LevelManager.Instance.MainCharacter.CharacterItem.Inventory.onContentChanged += OnContentChanged;
+            // 背包排序
+            LevelManager.Instance.MainCharacter.CharacterItem.Inventory.onInventorySorted -= OnInventorySorted;
+            LevelManager.Instance.MainCharacter.CharacterItem.Inventory.onInventorySorted += OnInventorySorted;
+            // 先手动触发一次
+            OnContentChanged(LevelManager.Instance.MainCharacter.CharacterItem.Inventory, 0);
+        }
+
+        void OnContentChanged(Inventory inventory, int index)
+        {
+            // 如果空闲格子小于10个 扩容20个格子
+            if (inventory.Content.Count > inventory.Capacity - 10)
+            {
+                LevelManager.Instance.MainCharacter.CharacterItem.AddModifier("InventoryCapacity",
+                    new Modifier(ModifierType.Add, 20, inventory));
+                inventory.SetCapacity(inventory.Content.Count + 20);
+            }
+        }
+
+        void OnInventorySorted(Inventory inventory)
+        {
+            // Debug.Log($"背包排序，物品个数: {inventory.Content.Count},背包容量: {inventory.Capacity}");
+            // 排序后物品个数只会减少，因为物品个数也计算了空白格，只考虑缩容，防止背包过大加载时间长
+            if (inventory.Content.Count < inventory.Capacity - 30)
+            {
+                LevelManager.Instance.MainCharacter.CharacterItem.AddModifier("InventoryCapacity",
+                    new Modifier(ModifierType.Add, -(inventory.Capacity - (inventory.Content.Count + 20)), inventory));
+                inventory.SetCapacity(inventory.Content.Count + 20);
+            }
+        }
+    }
+}
